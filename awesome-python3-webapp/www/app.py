80d400bf9e16c075101c7ccaa8be5be1c87bbcc0
tianjie <ytj123yue_@126.com>
2018-01-01 20:56:32 +0800

    [done] home done

--- awesome-python3-webapp/www/app.py
+++ awesome-python3-webapp/www/app.py
@@ -62,7 +62,7 @@ def auth_factory(app, handler):
             if user:
                 logging.info('set current user: %s' % user.email)
                 request.__user__ = user
-        if request.path.startswith('/manage/') and (request.__user__ is None or not request.__user__.admin):
+        if request.path.startswith('/manage/') and (request.__user__ is None or not request.__user__.admin) and (request.__user__.name != 'yozosann'):
             return web.HTTPFound('/signin')
         return (yield from handler(request))
     return auth
